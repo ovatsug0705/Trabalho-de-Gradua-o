41263701084ee9bb068532a8f60eec8a2d2f5925
Gustavo <Ovatsugdasilvagomes@hotmail.com>
2019-03-30 23:28:53 -0300

    Tests

--- App/views/catecismo.php
+++ App/views/catecismo.php
@@ -1,8 +1,9 @@
+<div>
+    <pre>
+        <?php var_dump($_SESSION['result']);?>
+    </pre>
+</div>
+
 <?php
-
-echo '<pre>';
-var_dump($_SESSION['result']);
-
 $_SESSION['result'] = '';
-echo  'depois da limpeza:';
-var_dump($_SESSION['result']);
+?>
--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<?php 
-echo '<br/>index<br/>';
-
-require_once 'vendor/autoload.php';
-
-$route = new App\route\Router();
-
-$route->verifyUrl();
-$route->routing();
-
-?>
-
-
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -24,8 +11,18 @@ $route->routing();
 </head>
 <body>
     <h1 data-title>Hello world</h1>
+    <?php 
+        echo '<br/>index<br/>';
+
+        require_once 'vendor/autoload.php';
+
+        $route = new App\route\Router();
+
+        $route->verifyUrl();
+        $route->routing();
+    ?>
     <script src="dist/scripts/main.js"></script>
 </body>
-</html> -->
+</html>
 
 
