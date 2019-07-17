72f43fee9b5c0459d41d92bcba4354c7ec19925d
Gustavo <Ovatsugdasilvagomes@hotmail.com>
2019-07-17 01:39:11 -0300

    Add header desktop style

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,6 @@ mix.js('src/scripts/app.js', 'public/dist/scripts/main.js')
    .sass('src/styles/main.scss', 'public/dist/styles/main.css')
    .copyDirectory('src/assets/fonts', 'public/dist/assets/fonts')
    .copyDirectory('src/assets/img', 'public/dist/assets/img')
+   .options({
+      processCssUrls: false
+   });
