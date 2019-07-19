b93e049aa2b349c1e13ca46a7db7deac361f3b0d
Gustavo <Ovatsugdasilvagomes@hotmail.com>
2019-07-19 01:11:05 -0300

    Add SCSS structure and change themes logic

--- src/scripts/modules/themes.js
+++ src/scripts/modules/themes.js
@@ -4,12 +4,62 @@ class Themes {
     this.elm2 = elm2;
     this.themes = [];
     this.setupListener();
+    this.changeTheme();
   }
 
   setupListener(){
     this.elm.addEventListener('click', ()=> this.elm2.classList.toggle("active"));
     this.themes = Array.from(this.elm2.querySelectorAll('[data-themes-color]'));
-    console.log('oi', this.themes);
+    this.themes.forEach(item => {
+      item.addEventListener('click', ()=> this.changeTheme());
+    });
+  }
+
+  changeTheme(){
+    let data = event ? event.currentTarget.dataset.themesColor : null;
+
+    this.themes.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    switch (data) {
+      case '0':
+        document.body.classList.remove('t-theme-01');
+        document.body.classList.remove('t-theme-02');
+        document.body.classList.remove('t-theme-03');
+        break;
+      case '1':
+        document.body.classList.add('t-theme-01');
+        document.body.classList.remove('t-theme-02');
+        document.body.classList.remove('t-theme-03');
+        break;
+      case '2':
+        document.body.classList.add('t-theme-02');
+        document.body.classList.remove('t-theme-01');
+        document.body.classList.remove('t-theme-03');
+        break;
+      case '3':
+        document.body.classList.add('t-theme-03');
+        document.body.classList.remove('t-theme-02');
+        document.body.classList.remove('t-theme-01');
+        break;
+      default:
+        if (localStorage.getItem('theme')) document.body.classList.add(localStorage.getItem('theme'));
+        break;
+    }
+
+    this.setActiveTheme(data);
+  }
+
+  setActiveTheme(data) {
+    if (data) {
+      localStorage.setItem('theme', `t-theme-0${data}`);
+      this.themes[data].classList.add('active');
+    } else if (localStorage.getItem('theme')) {
+      this.themes[localStorage.getItem('theme').substr(9, 1)].classList.add('active');
+    } else {
+      this.themes[0].classList.add('active');
+    }
   }
 }
 
